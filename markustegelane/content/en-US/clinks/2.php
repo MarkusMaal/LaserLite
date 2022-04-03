117d02dc37beeed26efccc5bb572cdaeaae6c4e4
MarkusMaal <markus.maal@gmail.com>
2022-04-03 13:36:04 +0300

    Some files that were missing, were added

---
+++ markustegelane/content/en-US/clinks/2.php
@@ -0,0 +1,10 @@
+<h2>Profiles</h2>
+<ul>
+<li>YouTube: <a href="https://www.youtube.com/MarkusTegelane">MarkusTegelane</a></li>
+<li>Twitter: <a href="https://twitter.com/@MarkusTegelane">@MarkusTegelane</a></li>
+<li>GitHub profile: <a href="https://github.com/MarkusMaal">MarkusMaal</a></li>
+<li>LBRY channel: <a href="https://lbry.tv/@MarkusTegelane:8">MarkusTegelane</a></li>
+<li>Videokeskkonnna BitChute channel: <a href="https://www.bitchute.com/channel/markustegelane/">MarkusTegelane</a></lI>
+<li>xda-developers profile: <a href="https://forum.xda-developers.com/member.php?u=8290831">TheMarkusGuy</a></li>
+</ul>
+<br/>
